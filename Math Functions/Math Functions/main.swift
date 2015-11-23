9ae36e7b1d02307ba5551025ccc9c84d70f88a95
kroldm <kroldm@gmail.com>
2015-11-23 20:58:41 +0200

    commit

--- Math Functions/Math Functions/main.swift
+++ Math Functions/Math Functions/main.swift
@@ -43,3 +43,25 @@ func product(x: Int, y: Int)->Int{
 }
 
 print(product(5, y: 8))
+
+func power(x: Int, y: Int)->Int?{
+    
+    if x == 0 {
+        return nil
+    }
+    
+    var res:Int = 1
+    
+    for var i = 0; i < y; i++ {
+        res *= x
+    }
+    
+    return res
+}
+
+//print(power(2, y: 3)!)
+
+let p = power(2, y: 0)
+if let valOfP = p {
+    print(valOfP)
+}
