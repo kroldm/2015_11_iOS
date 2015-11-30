588cd2b4d07285d791b235e26a5537ba1d6e8610
kroldm <kroldm@gmail.com>
2015-11-30 21:04:03 +0200

    structs

---
+++ Structs/Structs/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Structs
+//
+//  Created by HackerU on 30/11/2015.
+//  Copyright © 2015 HackerU. All rights reserved.
+//
+
+import Foundation
+
+struct Rectangle {
+    var x:Int = 0;
+    var y:Int = 0;
+    var width:Int = 0;
+    var heigth:Int = 0;
+}
+
+var myRect = Rectangle()
+myRect.heigth = 20
+myRect.x = 1
+
